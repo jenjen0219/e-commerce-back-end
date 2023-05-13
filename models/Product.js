fb545124e1d9714ed91c605b1aebe1b9be769297
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-05-12 20:09:36 -0700

    entered in the columns for the products model

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,45 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isDecimal: true
+      }
+    }, 
+    category_id: {
+      type: DataTypes.DECIMAL,
+      references: {
+        model: 'category',
+        key: 'id',
+        // unique is set to false since we want to be able to associate a category id to multiple products 
+        unique: false
+      }
+    }
   },
   {
     sequelize,
